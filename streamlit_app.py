5932702b0e30631bd5c0c7f10b9cfccb133a838b
Gajanan Todeti <114816136+TGajanan@users.noreply.github.com>
2024-09-20 17:31:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,13 @@ import pandas as pd
 st.title("Salary Projection for Next 5 Years")
 
 # Inputs: Current CTC and Average Hike %
-current_ctc = st.number_input("Enter your current CTC (Annual)", value=550000.0, step=10000.0)
+current_ctc = st.number_input("Enter your current CTC (Annual)", value=500000.0, step=10000.0)
 avg_hike = st.number_input("Enter Average Hike Percentage (%)", value=8.0, step=0.5)
 current_age = st.number_input("Enter your current age", value=26, step=1)
+professional_tax = st.number_input("Enter your states Professional tax", value=200, step=50)
+professional_tax = st.number_input("Enter your states Professional tax", value=200, step=50)
+employer_pf=st.number_input("Enter your employers PF",value=1800,steps=100)
+employee_pf=st.number_input("Enter your employee PF",value=1800,steps=100)
 
 # Convert hike to a multiplier
 hike_multiplier = 1 + (avg_hike / 100)
@@ -25,12 +29,13 @@ salary_data = {
 # Initial year and salary
 year = 2024
 salary_yearly = current_ctc
-
+Inhand_sal=(salary_yearly / 12) - professional_tax - employer_pf - employee_pf
 # Calculate the salary progression for 5 years
 for i in range(5):
     salary_data["Sr. No"].append(i + 1)
     salary_data["Salary Yearly"].append(round(salary_yearly, 2))
-    salary_data["Salary per month"].append(round(salary_yearly / 12, 2))
+    salary_data["Salary per month in Hand"].append(round(Inhand_sal), 2))
+    # salary_data["Salary per month"].append(round((salary_yearly / 12), 2))
     salary_data["Year"].append(year + i)
     salary_data["Hike Percentage"].append(round(avg_hike / 100, 2) if i != 0 else 0)
     salary_data["Age"].append(current_age + i)
